5901e96e7e91f8e6cb4beb849177368aebc984a9
Mark Krasny <krasnymark@gmail.com>
2019-09-13 20:48:05 -0400

    Bug fix - moves are behind history by 1

--- src/Game.js
+++ src/Game.js
@@ -43,7 +43,7 @@ export class Game extends React.Component {
   }
 
   handleClick(i, x, y) {
-    const moves = this.state.moves.slice(0, this.state.stepNumber + 1);
+    const moves = this.state.moves.slice(0, this.state.stepNumber);
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
